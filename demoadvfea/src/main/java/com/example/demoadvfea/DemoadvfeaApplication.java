fabb873fe1f3058b647236a39778e7fc733dc0dd
susmitha b <susmitha.billa@eagles.oc.edu>
2023-10-01 17:01:33 -0500

    Initial commit

---
+++ demoadvfea/src/main/java/com/example/demoadvfea/DemoadvfeaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoadvfea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoadvfeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoadvfeaApplication.class, args);
+	}
+
+}
---
+++ demoadvfea/src/test/java/com/example/demoadvfea/DemoadvfeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoadvfea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoadvfeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
